f094464b090535469f9622285f23a6ec442808a4
Kris Watson <kris@computestacks.com>
2022-03-17 09:19:02 -0700

    resolve bad path

--- php7.4-litespeed/51-cs-wp.sh
+++ php7.4-litespeed/51-cs-wp.sh
@@ -9,4 +9,4 @@ elif [[ ${CS_AUTH_KEY} -lt 20 ]]; then
   echo >&2 "CS_AUTH_KEY is too short to be real, generating random key..."
   export CS_AUTH_KEY=$(xxd -l24 -ps /dev/urandom | xxd -r -ps | base64 | tr -d = | tr + - | tr / _)
 fi
-sed -i "s/SET_CS_AUTH/$CS_AUTH_KEY/g" /usr/src/lsws/conf/httpd_config.conf
+sed -i "s/SET_CS_AUTH/$CS_AUTH_KEY/g" /usr/local/lsws/conf/httpd_config.conf
