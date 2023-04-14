4b17b69fdf65c0c002df5091a68af890e0ffbe53
Kris Watson <kris@computestacks.com>
2023-04-13 19:37:43 -0700

    Ensure vhost logging always exists. Ensure our plugin always exists

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # ComputeStacks Wordpress on OpenLiteSpeed Changelog
 
+## 2023-Apr-13
+
+* Auto-configure litespeed vhosts with latest configuration changes.
+* Automatically install the ComputeStacks wordpress plugin to ensure functionality with older sites.
+
+***
+
 ## 2023-Mar-10
 
 * Added PHP 8.2 
--- php8.0-litespeed/51-cs-wp.sh
+++ php8.0-litespeed/51-cs-wp.sh
@@ -13,11 +13,88 @@ sed -i "s/CS_AUTH_KEY=.*/CS_AUTH_KEY=$CS_AUTH_KEY/g" /usr/local/lsws/conf/httpd_
 # if it does not exist, create it.
 grep -q -e 'CS_AUTH_KEY' /usr/local/lsws/conf/httpd_config.conf || sed -i "/extprocessor lsphp/a  env                     CS_AUTH_KEY=$CS_AUTH_KEY" /usr/local/lsws/conf/httpd_config.conf
 
-if [ -f /var/www/html/wordpress/wp-content/mu-plugins/cstacks-config.php ]; then
+if [ -f "/opt/cs-wordpress-plugin-main/cstacks-config.php" ]; then
   echo >&2 "Updating ComputeStacks integration with latest version..."
-  rm -f /var/www/html/wordpress/wp-content/mu-plugins/cstacks-config.php
   sudo -u www-data cp /opt/cs-wordpress-plugin-main/cstacks-config.php /var/www/html/wordpress/wp-content/mu-plugins/
-  grep -q -e 'CS_PLUGIN_DIR' /var/www/html/wordpress/wp-config.php || sed -i "/WP_MEMORY_LIMIT/a define( 'CS_PLUGIN_DIR', '/opt/cs-wordpress-plugin-main' );" /var/www/html/wordpress/wp-config.php
+  sudo -u www-data wp config set CS_PLUGIN_DIR /var/www/html/wordpress/wp-config.php
+fi
+
+echo >&2 "Updating litespeed configuration..."
+
+[ -f /usr/local/lsws/conf/vhosts/Wordpress/htpasswd ] || touch /usr/local/lsws/conf/vhosts/Wordpress/htpasswd
+chmod 644 /usr/local/lsws/conf/vhosts/Wordpress/htpasswd
+chown lsadm:lsadm /usr/local/lsws/conf/vhosts/Wordpress/htpasswd
+
+[ -f /usr/local/lsws/conf/vhosts/Wordpress/htgroup ] || touch /usr/local/lsws/conf/vhosts/Wordpress/htgroup
+chmod 644 /usr/local/lsws/conf/vhosts/Wordpress/htgroup
+chown lsadm:lsadm /usr/local/lsws/conf/vhosts/Wordpress/htgroup
+
+if grep -Fq 'errorlog' /usr/local/lsws/conf/vhosts/Wordpress/vhconf.conf; then
+  echo "errorlog configuration found, skipping..."
 else
-  echo >&2 "ComputeStakcs mu-plugin missing, skipping installation..."
+  cat << EOF >> '/usr/local/lsws/conf/vhosts/Wordpress/vhconf.conf'
+errorlog /var/www/logs/error.log {
+  useServer               0
+  logLevel                NOTICE
+  rollingSize             10M
+  keepDays                30
+  compressArchive         0
+}
+EOF
+fi 
+
+if grep -Fq 'accesslog' /usr/local/lsws/conf/vhosts/Wordpress/vhconf.conf; then
+  echo "accesslog configuration found, skipping..."
+else
+  cat << EOF >> '/usr/local/lsws/conf/vhosts/Wordpress/vhconf.conf'
+accesslog /var/www/logs/access.log {
+  useServer               0
+  logFormat               "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"
+  logHeaders              7
+  rollingSize             10M
+  keepDays                30
+  compressArchive         0
+}
+EOF
+fi 
+
+if grep -Fq 'realm protected' /usr/local/lsws/conf/vhosts/Wordpress/vhconf.conf; then
+  echo "protected realm found, skipping..."
+else
+  cat << EOF >> '/usr/local/lsws/conf/vhosts/Wordpress/vhconf.conf'
+realm protected {
+  
+  userDB  {
+    location              \$SERVER_ROOT/conf/vhosts/\$VH_NAME/htpasswd
+  }
+
+  groupDB  {
+    location              \$SERVER_ROOT/conf/vhosts/\$VH_NAME/htgroup
+  }
+}
+EOF
+fi 
+
+
+if grep -Fq 'xmlrpc' /usr/local/lsws/conf/vhosts/Wordpress/vhconf.conf; then
+  echo "xmlrpc block found, skipping..."
+else
+  cat << EOF >> '/usr/local/lsws/conf/vhosts/Wordpress/vhconf.conf'
+context /xmlrpc.php {
+  allowBrowse             0
+
+  accessControl  {
+    deny                  0.0.0.0/0
+  }
+
+  rewrite  {
+
+  }
+  addDefaultCharset       off
+
+  phpIniOverride  {
+
+  }
+}
+EOF
 fi
