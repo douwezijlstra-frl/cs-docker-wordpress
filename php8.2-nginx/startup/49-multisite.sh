45715ff562cf42f515bdeaf783561aab9908487c
Douwe Zijlstra <douwe@xxlhosting.nl>
2023-06-19 18:52:12 +0200

    Added suggestion to check if WordPress is installed

--- php8.2-nginx/startup/49-multisite.sh
+++ php8.2-nginx/startup/49-multisite.sh
@@ -1,17 +1,22 @@
-# if wp config get MULTISITE returns 'true', then multisite is enabled
-if [[ $(sudo -u www-data wp config get MULTISITE) == "1" ]]; then
-    
-# if wp config get SUBDOMAIN_INSTALL returns 'true', then subdomain is enabled
-if [[ $(sudo -u www-data wp config get SUBDOMAIN_INSTALL) == "1" ]]; then
-    echo >&2 "Subdomain multisite is enabled - installing config accordingly"
-    mv /etc/nginx/sites-available/wordpress-subdomain.conf /etc/nginx/sites-enabled/default
-    return 0
-else
-    echo >&2 "Subdirectory multisite is enabled - installing config accordingly"
-    mv /etc/nginx/sites-available/wordpress-subdirectory.conf /etc/nginx/sites-enabled/default
-    return 0
-fi
-else
-    echo >&2 "Multisite is disabled - continuing..."
-    return 1
+# check if wordpress is installed, else continue
+if [ -f /var/www/html/wordpress/wp-config.php ]; then
+
+    # if wp config get MULTISITE returns 'true', then multisite is enabled
+    if [[ $(sudo -u www-data wp --path=/var/www/html/wordpress config get MULTISITE) == "1" ]]; then
+        
+    # if wp config get SUBDOMAIN_INSTALL returns 'true', then subdomain is enabled
+    if [[ $(sudo -u www-data wp --path=/var/www/html/wordpress config get SUBDOMAIN_INSTALL) == "1" ]]; then
+        echo >&2 "Subdomain multisite is enabled - installing config accordingly"
+        mv /etc/nginx/sites-available/wordpress-subdomain.conf /etc/nginx/sites-enabled/default
+        return 0
+    else
+        echo >&2 "Subdirectory multisite is enabled - installing config accordingly"
+        mv /etc/nginx/sites-available/wordpress-subdirectory.conf /etc/nginx/sites-enabled/default
+        return 0
+    fi
+    else
+        echo >&2 "Multisite is disabled - continuing..."
+        return 1
+    fi
+
 fi
